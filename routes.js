dcbb19b85b4a03a29f470b270fabdeb968af0687
jstpierre24 <james@studiostpierre.com>
2016-11-10 15:48:45 -0500

    changed routing setup to handle file separation, refer to route.js and login.js for example

--- routes.js
+++ routes.js
@@ -5,14 +5,14 @@ exports = module.exports = function (app, router) {
       res.json({ message: 'hooray! welcome to our api!' });
     });
 
-    var rooms = require('./routes/rooms')
-    var users = require('./routes/users')
-    var inventory = require('./routes/inventory')
-    var login = require('./routes/login')
+    var rooms = require('./routes/rooms')(app, router);
+    var users = require('./routes/users')(app, router);
+    var inventory = require('./routes/inventory')(app, router);
+    var login = require('./routes/login')(app, router);
 
 ///////////////////////
 
-    app.use('/', router);
+
 
     // Example: (you can add /:id at the end of each routes)
     // /rooms/conference
@@ -20,12 +20,14 @@ exports = module.exports = function (app, router) {
     // /rooms/office
     // /rooms/furniture
 
-    app.use('/rooms/:room_type/:room_id', rooms)  // room_id is optional (changes behaviour of HTTP verbs)
+    // app.use('/rooms/:room_type/:room_id', rooms)  // room_id is optional (changes behaviour of HTTP verbs)
+    // app.use('/rooms/:room_type/:room_id', rooms)  // room_id is optional (changes behaviour of HTTP verbs)
+
 
     // Example: (you can add /:id at the end of each routes)
     // /users/it/:id
     // /users/employee/:id
-    app.use('/users/:user_type/:user_id', users)  // not sure we need this
+    // app.use('/users/:user_type/:user_id', users)  // not sure we need this
 
     // Example: (you can add /:id at the end of each routes)
     // /inventory/computers
@@ -33,8 +35,9 @@ exports = module.exports = function (app, router) {
     // /inventory/networks
     // /inventory/whiteboards
     // /inventory/flipcharts
-    app.use('/inventory/:resource_type/:resource_id', inventory) // resource_id is optional (changes the behaviour of HTTP verbs)
-    app.use('/login', login)  // login route
+    // app.use('/inventory/:resource_type/:resource_id', inventory) // resource_id is optional (changes the behaviour of HTTP verbs)
+
+    app.use('/', router);
 
 /////////////////////////
 };
--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,9 @@
 module.exports = function (app, router) {
 
-
+  router.route('/login')
+    .get(function(req, res) {
+            res.json({ message: 'At login created!' });
+    });
 
 
 }
