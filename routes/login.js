2059b516ff1b90114b279a8b40f8ac7c30dce8e4
jstpierre24 <james@studiostpierre.com>
2016-11-10 15:55:05 -0500

    Login dummy file created

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,15 @@ module.exports = function (app, router) {
 
   router.route('/login')
     .get(function(req, res) {
-            res.json({ message: 'At login created!' });
+      if(true){
+        //place redirect page here
+        // res.redirect("/admin");
+        res.json({ message: 'Logged in' });
+      }
+      else{
+        res.json({ message: 'Invalid login' });
+      }
+
     });
 
 
