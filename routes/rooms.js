49e1fec9476b18ef5b2ee6816de240fa73b7a526
jstpierre24 <james@studiostpierre.com>
2016-11-10 20:48:14 -0500

    andrea caught and implemented the return for all rooms of a certain type

--- routes/rooms.js
+++ routes/rooms.js
@@ -8,17 +8,23 @@ module.exports = function (app, router) {
       res.json({ status: true, body: 'Return JSON object instead of this string' });
     });
 
-  router.route('/rooms/:room_type/:room_id')
+  router.route('/rooms/:room_type')
     .get(function(req, res) {
       //return room with req.params.room_type and req.params.room_id
       res.json({ status: true, body: 'Return JSON object instead of this string' });
+    })
+
+  router.route('/rooms/:room_type/:room_id')
+    .get(function(req, res) {
+      //return room with req.params.room_type and req.params.room_id
+      res.json({ status: true, body: req.params.room_type + " " + req.params.room_id});
     })
     .post(function(req, res) {
       //add item to DB with req.params.room_type and req.params.room_id
     })
     .delete(function(req, res) {
       //Delete item from DB with req.params.room_type and req.params.room_id
-    })
+    });
 
 
 
