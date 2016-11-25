255d5a31da4ec70c8582d86eeb6ce7adb8f79a1e
TashaChrispy <natasha.chrispin2403@hotmail.com>
2016-11-25 01:16:20 -0500

    Create/Delete user
    Create user / delete user implemented.

--- routes/users.js
+++ routes/users.js
@@ -20,8 +20,22 @@ module.exports = function (app, router, db, models) {
       // TODO: create a new user
       // Check if user creation is from an unregistered user or an admin
       // Assume Admin creation initially, implement registration later
-
-    })
+      
+      var newUser = req.body.user;
+      var user_type = req.body.user_type;
+      var email = req.body.email;
+      var validator = require('validator'); 
+      
+      if (!validator.isEmail(email))
+      {
+         res.json({ status: false, body: "The email provided is an invalid format." });
+      }
+      else{
+       db.addUser(user_type, newUser).then(result => {
+       res.json({ status: true, body: "Successfully Created User" });
+      });
+     }
+   })
 
 
   router.route('/users/:user_id')
@@ -42,20 +56,27 @@ module.exports = function (app, router, db, models) {
       //TODO this is for modification
       //ID is autoincrement here. Therefore, not the right route
       // TODO Refactor, the type is taken from the body
-      db.addUser(req.params.user_type, req.body.user).then(result => {
+      //db.modifyUser(req.params.user_type, req.body.user).then(result => {
         /*TODO HERE Check if return is true or false*/
         res.json({ status: true, body: "some message" });
       });
       //add user to DB with req.params.user_type and req.params.user_id
       // Might not implement this from the perspective of a users route.. Maybe /signup or /register route
       // or do both (IT employee/manager wants to create a user without going through signup/register...?)
-    })
+    }
     .delete(function(req, res) {
-      db.removeUser(req.params.user_id).then(result => {
-        /*HERE Check if return is true or false*/
-        res.json({ status: true, body: "some message" });
-      });
+      db.removeUser(req.params.user_type, req.params.user_id).then(result => {
       //Delete user from DB with req.params.user_type and req.params.user_id
+      /*Assuming that only admins can delete users for now*/
+      if(user_type=="admin"){
+
+        res.json({ status: true, body: "User successfully deleted." });
+      }
+      else{
+        res.json({ status: false, body: "Unable to delete user." });
+      }
+      });
+      
     })
 
 
