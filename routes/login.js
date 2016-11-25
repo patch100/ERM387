5a0f9a916bf18cbe9269011b8edc32f0c9af9f62
Andrea-B <a.biondi09@gmail.com>
2016-11-25 11:20:20 -0500

    Login

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,6 @@ module.exports = function (app, router, db, models) {
   router.route('/login')
 	.post(function(req, res){ 
 		
-<<<<<<< HEAD
 		// clean input ? *********************************
 			// for example: check for no spaces (simplified version)
 		try {
@@ -12,16 +11,6 @@ module.exports = function (app, router, db, models) {
 		    .then(
 				responseObject => {
 			// Convert responseObject to boolean ***************************
-=======
-		// clean input ?
-			// for example: check for no spaces (simplified version)
-		try {
-        models.sequelize.sync().then(() => {
-          db.userLogin(req.params.user_id, req.params.user_password)) // get user/pass retreival method from db  ***********
-		    .then(
-				responseObject => {
-			// Convert responseObject to boolean
->>>>>>> origin/feature/login
 			// If successful (User exists, and password matches)
 				
 					// create json web token
@@ -31,37 +20,14 @@ module.exports = function (app, router, db, models) {
 
 					res.json({ 
 						status: true,
-<<<<<<< HEAD
 						body: { token: userToken, 
 								message: "Successful Login!"}
-=======
-						body: {message: "Successful Login!"},
-						token: userToken
->>>>>>> origin/feature/login
 					});
 					
 				}
             )
           });
 		} catch (e) {
-<<<<<<< HEAD
-=======
-        console.log(e);
-        res.json({status: false, body: {error: "message"}})
-      }
-	
-	});
-	///////////////////
-	
-  router.route('/test')
-    .get(function(req, res) {
-      try {
-        models.sequelize.sync(/*{force:true}*/).then(() => {
-          db.getRoomItems(1)
-            .then(aa => res.json({ status: true, body: aa }))
-        });
-      } catch (e) {
->>>>>>> origin/feature/login
         console.log(e);
         res.json({status: false, body: {error: "message"}})
       }
