31d17828579075e169a653ad34f5397d02715e4c
Andrea-B <a.biondi09@gmail.com>
2016-11-25 10:32:12 -0500

    Add modified login

--- routes/login.js
+++ routes/login.js
@@ -1,18 +1,49 @@
 module.exports = function (app, router, db, models) {
 
   router.route('/login')
-    .get(function(req, res) {
-      if(true){
-        //place redirect page here
-        // res.redirect("/admin");
-        res.json({status: true, body: {message: "Successful Login!"} });
+	.post(function(req, res){ 
+		
+		// clean input ?
+			// for example: check for no spaces (simplified version)
+		try {
+        models.sequelize.sync().then(() => {
+          db.userLogin(req.params.user_id, req.params.user_password)) // get user/pass retreival method from db  ***********
+		    .then(
+				responseObject => {
+			// Convert responseObject to boolean
+			// If successful (User exists, and password matches)
+				
+					// create json web token
+					var userToken = jwt.sign(user, app.get('superSecret'), {
+						expiresInMinutes: 1440 // expires in 24 hours
+					});
+					// add token to response ? (or redirect)
+					res.json({ 
+						status: true,
+						body: {message: "Successful Login!"},
+						token: userToken // do we need this?
+					});
+					
+					// redirect to page corresponding to user type	
+						// example:
+					res.redirect('/resources');
+					
+					// redirect the user to the appropriate page (depending on type of user)
+			// If failure (User exists, password does not match OR User does not exist)
+					// add error to response
+					// redirect to login page
+					res.redirect('/');
+				}
+            )
+          });
+		} catch (e) {
+        console.log(e);
+        res.json({status: false, body: {error: "message"}})
       }
-      else{
-        res.json({status: false, body: {error: "Failed Login.."} });
-      }
-
-    });
-
+	
+	});
+	///////////////////
+	
   router.route('/test')
     .get(function(req, res) {
       try {
