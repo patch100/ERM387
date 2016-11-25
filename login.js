b077d3143e4065d9ce78ced2b2741961dcea59c9
Andrea-B <a.biondi09@gmail.com>
2016-11-25 11:08:07 -0500

    Modify body with message and token

---
+++ login.js
@@ -0,0 +1,52 @@
+module.exports = function (app, router, db, models) {
+
+  router.route('/login')
+	.post(function(req, res){ 
+		
+		// clean input ?
+			// for example: check for no spaces (simplified version)
+		try {
+        models.sequelize.sync().then(() => {
+          db.userLogin(req.body.user_id, req.body.user_password)) // get user/pass retreival method from db  ***********
+		    .then(
+				responseObject => {
+			// Convert responseObject to boolean
+			// If successful (User exists, and password matches)
+				
+					// create json web token
+					var userToken = jwt.sign(user, app.get('superSecret'), {
+						expiresInMinutes: 1440 // expires in 24 hours
+					});
+
+					res.json({ 
+						status: true,
+						body: { token: userToken, 
+								message: "Successful Login!"}
+					});
+					
+				}
+            )
+          });
+		} catch (e) {
+        console.log(e);
+        res.json({status: false, body: {error: "message"}})
+      }
+	
+	});
+	///////////////////
+	
+  router.route('/test')
+    .get(function(req, res) {
+      try {
+        models.sequelize.sync(/*{force:true}*/).then(() => {
+          db.getRoomItems(1)
+            .then(aa => res.json({ status: true, body: aa }))
+        });
+      } catch (e) {
+        console.log(e);
+        res.json({ status: false, body: {error: "message"} });
+      }
+
+    });
+
+}
