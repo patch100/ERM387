d3ee1201880f3101100507996283d29012583e4e
Andrea-B <a.biondi09@gmail.com>
2016-11-25 11:14:34 -0500

    Login

--- routes/login.js
+++ routes/login.js
@@ -1,30 +1,37 @@
 module.exports = function (app, router, db, models) {
 
   router.route('/login')
-    .get(function(req, res) {
-      if(true){
-        //place redirect page here
-        // res.redirect("/admin");
-        res.json({status: true, body: {message: "Successful Login!"} });
-      }
-      else{
-        res.json({status: false, body: {error: "Failed Login.."} });
-      }
+	.post(function(req, res){ 
+		
+		// clean input ? *********************************
+			// for example: check for no spaces (simplified version)
+		try {
+        models.sequelize.sync().then(() => {
+          db.userLogin(req.body.user_id, req.body.user_password)) // get user/pass retreival method from db  
+		    .then(
+				responseObject => {
+			// Convert responseObject to boolean ***************************
+			// If successful (User exists, and password matches)
+				
+					// create json web token
+					var userToken = jwt.sign(user, app.get('superSecret'), {
+						expiresInMinutes: 1440 // expires in 24 hours
+					});
 
-    });
-
-  router.route('/test')
-    .get(function(req, res) {
-      try {
-        models.sequelize.sync(/*{force:true}*/).then(() => {
-          db.getRoomItems(1)
-            .then(aa => res.json({ status: true, body: aa }))
-        });
-      } catch (e) {
+					res.json({ 
+						status: true,
+						body: { token: userToken, 
+								message: "Successful Login!"}
+					});
+					
+				}
+            )
+          });
+		} catch (e) {
         console.log(e);
-        res.json({ status: false, body: {error: "message"} });
+        res.json({status: false, body: {error: "message"}})
       }
-
-    });
+	
+	});
 
 }
