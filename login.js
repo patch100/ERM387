ec94deb8d168d34e50dd1097ba6265a988aa1c9c
Andrea-B <a.biondi09@gmail.com>
2016-12-01 15:34:37 -0500

    Added cookie

--- login.js
+++ login.js
@@ -1,40 +1,45 @@
 module.exports = function (app, router, db, models) {
 
   router.route('/login')
-	.post(function(req, res){ 
-		
+	.post(function(req, res){
+
 		// clean input ?
 			// for example: check for no spaces (simplified version)
 		try {
         models.sequelize.sync().then(() => {
-          db.userLogin(req.body.user_id, req.body.user_password)) // get user/pass retreival method from db  ***********
+          db.userLogin(req.body.user_id, req.body.user_password))
 		    .then(
 				responseObject => {
-			// Convert responseObject to boolean
-			// If successful (User exists, and password matches)
-				
-					// create json web token
-					var userToken = jwt.sign(user, app.get('superSecret'), {
-						expiresInMinutes: 1440 // expires in 24 hours
-					});
 
-					res.json({ 
-						status: true,
-						body: { token: userToken, 
-								message: "Successful Login!"}
-					});
-					
-				}
+			     if(responseObject){ // If successful (User exists, and password matches)
+
+        					// create json web token
+        					var userToken = jwt.sign(user, app.get('superSecret'), {
+        						expiresInMinutes: 1440 // expires in 24 hours
+        					});
+
+                  res.cookie('token', userToken)
+
+        					res.json({
+        						status: true,
+        						body: { message: "Successful Login!"}
+        					});
+                  
+          }else{  // Username & password are invalid
+                  res.json({status: false, body: {error: "message"}})
+
+        }
+				} // end of responseObject
             )
           });
 		} catch (e) {
         console.log(e);
-        res.json({status: false, body: {error: "message"}})
+        res.json({status: false, body: {error: "message"}}) // all fails
       }
-	
+
 	});
 	///////////////////
-	
+
   router.route('/test')
     .get(function(req, res) {
       try {
