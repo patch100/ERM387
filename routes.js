9537852f5662fafe3cb9199e8b60b7fcfe0a41b2
Andrea-B <a.biondi09@gmail.com>
2016-12-01 15:59:04 -0500

    app.all('')

--- routes.js
+++ routes.js
@@ -1,45 +1,53 @@
 exports = module.exports = function (app, router, db, models) {
 
 
-    router.get('/', function(req, res) {
-      res.json({ message: 'Welcome to our API!' });
-    });
+  router.get('/', function(req, res) {
+    res.json({ message: 'Welcome to our API!' });
+  });
 
-	var login = require('./routes/login')(app, router, db, models);
+  var login = require('./routes/login')(app, router, db, models);
 
-	router.use(function(req, res, next) {
-		// check header or url parameters or post parameters for token
-		var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  router.use(function(req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['token'] || req.cookies.token;
 
-		// decode token
-		if (token) {
-			// verifies secret and checks exp
-			jwt.verify(token, app.get('superSecret'), function(err, decoded) {
-			  if (err) {
-				return res.json({ success: false, message: 'Failed to authenticate token.' });
-			  } else {
-				// if everything is good, save to request for use in other routes
-				req.decoded = decoded;
-				next();
-			  }
-			});
+    // decode token
+    if (token) {
+      // verifies secret and checks exp
+      jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+        if (err) {
+          return res.json({ success: false, message: 'Failed to authenticate token.' });
+        } else {
+          // if everything is good, save to request for use in other routes
+          req.decoded = decoded;
+          next();
+        }
+      });
 
-		} else {
-			// if there is no token, return an error
-			return res.status(403).send({
-				success: false,
-				message: 'No token provided.'
-			});
+    } else {
+      // if there is no token, return an error
+      return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+      });
 
-		}
-	});
+    }
+  });
+
+  function requireAuthentication(){
+    if (req.cookies.token){ // ****************************
+
+    }
+  }
+
+  app.all('/inventory', requireAuthentication)
+  app.all('/rooms', requireAuthentication)
 
 
+  var rooms = require('./routes/rooms')(app, router, db, models);
+  var users = require('./routes/users')(app, router, db, models);
+  var inventory = require('./routes/inventory')(app, router, db, models);
 
-    var rooms = require('./routes/rooms')(app, router, db, models);
-    var users = require('./routes/users')(app, router, db, models);
-    var inventory = require('./routes/inventory')(app, router, db, models);
-
-    app.use('/', router);
+  app.use('/', router);
 
 };
