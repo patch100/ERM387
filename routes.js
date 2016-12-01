3d5b4e9d48ba95b9a08ed37fc03a5eb5d4696e4e
Andrea-B <a.biondi09@gmail.com>
2016-12-01 15:37:17 -0500

    token

--- routes.js
+++ routes.js
@@ -1,14 +1,44 @@
-exports = module.exports = function (app, router, db) {
+exports = module.exports = function (app, router, db, models) {
 
 
     router.get('/', function(req, res) {
       res.json({ message: 'Welcome to our API!' });
     });
 
-    var rooms = require('./routes/rooms')(app, router, db);
-    var users = require('./routes/users')(app, router, db);
-    var inventory = require('./routes/inventory')(app, router, db);
-    var login = require('./routes/login')(app, router, db);
+	var login = require('./routes/login')(app, router, db, models);
+
+	router.use(function(req, res, next) {
+		// check header or url parameters or post parameters for token
+		var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+		// decode token
+		if (token) {
+			// verifies secret and checks exp
+			jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+			  if (err) {
+				return res.json({ success: false, message: 'Failed to authenticate token.' });
+			  } else {
+				// if everything is good, save to request for use in other routes
+				req.decoded = decoded;
+				next();
+			  }
+			});
+
+		} else {
+			// if there is no token, return an error
+			return res.status(403).send({
+				success: false,
+				message: 'No token provided.'
+			});
+
+		}
+	});
+
+
+
+    var rooms = require('./routes/rooms')(app, router, db, models);
+    var users = require('./routes/users')(app, router, db, models);
+    var inventory = require('./routes/inventory')(app, router, db, models);
 
     app.use('/', router);
 
