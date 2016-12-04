7300f21419fa729b250da2e2c51cea86bb7bb208
Greg <houle.greg@gmail.com>
2016-12-04 15:20:07 -0500

    fixed parens mismatch
    Signed-off-by: Greg <houle.greg@gmail.com>

--- routes/login.js
+++ routes/login.js
@@ -1,61 +1,58 @@
-module.exports = function (app, router, db, models) {
+module.exports = function(app, router, db, models) {
 
-  router.route('/login')
-  .post(function(req, res){
+    router.route('/login')
+        .post(function(req, res) {
 
-    // clean input ?
-    // for example: check for no spaces (simplified version)
-    try {
-      models.sequelize.sync().then(() => {
-        db.userLogin(req.body.user_id, req.body.user_password))
-        .then(
-          responseObject => {
+            // clean input ?
+            // for example: check for no spaces (simplified version)
+            try {
+                db.userLogin(req.body.user_id, req.body.user_password)
+                    .then(
+                        responseObject => {
+                            if (responseObject.status) { // If successful (User exists, and password matches)
+                                // create json web token
+                                var userToken = jwt.sign(user, app.get('superSecret'), {
+                                    expiresInMinutes: 1440 // expires in 24 hours
+                                });
 
-            if(responseObject.status){ // If successful (User exists, and password matches)
+                                res.cookie('token', userToken)
 
-              // create json web token
-              var userToken = jwt.sign(user, app.get('superSecret'), {
-                expiresInMinutes: 1440 // expires in 24 hours
-              });
+                                res.json({
+                                    status: true,
+                                    body: {
+                                        message: "Successful Login!",
+                                        isAdmin: responseObject.is_admin
+                                    } // ***************************************
+                                });
 
-              res.cookie('token', userToken)
-
-              res.json({
-                status: true,
-                body: { message: "Successful Login!",
-                        isAdmin: responseObject.is_admin } // ***************************************
-              });
-
-            }else{  // Username & password are invalid
-              res.json({
-                status: false,
-                body: {error: "message"}
-              })
-
+                            } else { // Username & password are invalid
+                                res.json({
+                                    status: false,
+                                    body: { error: "Failure to Authenticate" }
+                                });
+                            }
+                        } // end of responseObject
+                    );
+            } catch (e) {
+                console.log(e);
+                res.json({ status: false, body: { error: "Request Failure" } }) // all fails
             }
-          } // end of responseObject
-        )
-      });
-    } catch (e) {
-      console.log(e);
-      res.json({status: false, body: {error: "message"}}) // all fails
-    }
 
-  });
-  ///////////////////
+        });
+    ///////////////////
 
-  router.route('/test')
-  .get(function(req, res) {
-    try {
-      models.sequelize.sync(/*{force:true}*/).then(() => {
-        db.getRoomItems(1)
-        .then(aa => res.json({ status: true, body: aa }))
-      });
-    } catch (e) {
-      console.log(e);
-      res.json({ status: false, body: {error: "message"} });
-    }
+    router.route('/test')
+        .get(function(req, res) {
+            try {
+                models.sequelize.sync( /*{force:true}*/ ).then(() => {
+                    db.getRoomItems(1)
+                        .then(aa => res.json({ status: true, body: aa }))
+                });
+            } catch (e) {
+                console.log(e);
+                res.json({ status: false, body: { error: "message" } });
+            }
 
-  });
+        });
 
 }
