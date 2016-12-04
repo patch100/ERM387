b17ac3609c402834f22ce73a35ccd92ee8279968
Greg <houle.greg@gmail.com>
2016-12-04 17:03:05 -0500

    fixed parens issue (merge bug)
    Signed-off-by: Greg <houle.greg@gmail.com>

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 module.exports = function(app, router, db) {
 
-  // /users/:user_type/:user_id'  // not sure we need this
+    // /users/:user_type/:user_id'  // not sure we need this
 
-  router.route('/users')
-    .get(function(req, res) {
-      //return all users
+    router.route('/users')
+        .get(function(req, res) {
+            //return all users
             var filters = req.query;
 
             //TODO prefix email?
@@ -15,32 +15,30 @@ module.exports = function(app, router, db) {
                     body: users
                 }))
 
-    })
-    .post(function(req, res) {
-      // TODO: Create a new user
-      // Check if user creation is from an unregistered user or an admin
-      // Assume Admin creation initially, implement registration later
-      
-      var newUser = req.body.user;
-      var user_type = req.body.user_type;
-      var email = req.body.email;
-      var validator = require('validator'); 
-      
-      if (!validator.isEmail(email))
-      {
-         res.json({ status: false, body: "The email provided is an invalid format." });
-      }
-      else{
-       db.addUser(user_type, newUser).then(result => {
-       res.json({ status: true, body: "Successfully Created User" });
-      });
-     }
-   });
+        })
+        .post(function(req, res) {
+            // TODO: Create a new user
+            // Check if user creation is from an unregistered user or an admin
+            // Assume Admin creation initially, implement registration later
+
+            var newUser = req.body.user;
+            var user_type = req.body.user_type;
+            var email = req.body.email;
+            var validator = require('validator');
+
+            if (!validator.isEmail(email)) {
+                res.json({ status: false, body: "The email provided is an invalid format." });
+            } else {
+                db.addUser(user_type, newUser).then(result => {
+                    res.json({ status: true, body: "Successfully Created User" });
+                });
+            }
+        });
 
 
-  router.route('/users/:user_id')
-    .get(function(req, res) {
-      //return user with req.params.user_type and req.params.user_id *** not sure if needed
+    router.route('/users/:user_id')
+        .get(function(req, res) {
+            //return user with req.params.user_type and req.params.user_id *** not sure if needed
 
             var filters = req.query;
             //TODO prefix email?
@@ -51,36 +49,30 @@ module.exports = function(app, router, db) {
                     body: user
                 }))
 
-    })
-    .post(function(req, res) {
-      //TODO This is to modify a user.
-            //ID is autoincrement here. Therefore, not the right route
-            // TODO Refactor, the type is taken from the body
-      db.updateUser(req.params.user_id, req.body.user).then(result => {
-        /*TODO HERE Check if return is true or false*/
-        if(result){
-          res.json({ status: true, body: "User successfully modified." });
-                    body: "some message"
-                });
-        } else {
-          res.json({ status: false, body: "We're sorry, the specified user could not be modified." });
-        }
-      })
-      
-     //Delete user from DB with req.params.user_type and req.params.user_id
-      /*Assuming that only admins can delete users for now*/
-    .delete(function(req, res) {
-      db.removeUser(req.params.user_id).then(result => {
-      if(user_type=="admin"){
+        })
+        .post(function(req, res) {
+            db.updateUser(req.params.user_id, req.body.user).then(result => {
+                /*TODO HERE Check if return is true or false*/
+                if (result) {
+                    res.json({ status: true, body: "User successfully modified." });
+                } else {
+                    res.json({ status: false, body: "We're sorry, the specified user could not be modified." });
+                }
+            })
+        })
+        //Delete user from DB with req.params.user_type and req.params.user_id
+        /*Assuming that only admins can delete users for now*/
+        .delete(function(req, res) {
+            db.removeUser(req.params.user_id).then(result => {
+                if (user_type == "admin") {
 
-        res.json({ status: true, body: "User successfully deleted." });
-      }
-      else{
-        res.json({ status: false, body: "Unable to delete user." });
-      }
-      });
-      
-    })
+                    res.json({ status: true, body: "User successfully deleted." });
+                } else {
+                    res.json({ status: false, body: "Unable to delete user." });
+                }
+            });
+
+        })
 
 
 }
