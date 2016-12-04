06db0a3c53d386f63764e5e41637950a2ff2578e
Andrea-B <a.biondi09@gmail.com>
2016-12-03 20:49:13 -0500

    fixed login file in routes folder, deleted login file from main

--- routes/login.js
+++ routes/login.js
@@ -1,37 +1,61 @@
 module.exports = function (app, router, db, models) {
 
   router.route('/login')
-	.post(function(req, res){ 
-		
-		// clean input ? *********************************
-			// for example: check for no spaces (simplified version)
-		try {
-        models.sequelize.sync().then(() => {
-          db.userLogin(req.body.user_id, req.body.user_password)) // get user/pass retreival method from db  
-		    .then(
-				responseObject => {
-			// Convert responseObject to boolean ***************************
-			// If successful (User exists, and password matches)
-				
-					// create json web token
-					var userToken = jwt.sign(user, app.get('superSecret'), {
-						expiresInMinutes: 1440 // expires in 24 hours
-					});
+  .post(function(req, res){
 
-					res.json({ 
-						status: true,
-						body: { token: userToken, 
-								message: "Successful Login!"}
-					});
-					
-				}
-            )
-          });
-		} catch (e) {
-        console.log(e);
-        res.json({status: false, body: {error: "message"}})
-      }
-	
-	});
+    // clean input ?
+    // for example: check for no spaces (simplified version)
+    try {
+      models.sequelize.sync().then(() => {
+        db.userLogin(req.body.user_id, req.body.user_password))
+        .then(
+          responseObject => {
+
+            if(responseObject.status){ // If successful (User exists, and password matches)
+
+              // create json web token
+              var userToken = jwt.sign(user, app.get('superSecret'), {
+                expiresInMinutes: 1440 // expires in 24 hours
+              });
+
+              res.cookie('token', userToken)
+
+              res.json({
+                status: true,
+                body: { message: "Successful Login!",
+                        isAdmin: responseObject.is_admin } // ***************************************
+              });
+
+            }else{  // Username & password are invalid
+              res.json({
+                status: false,
+                body: {error: "message"}
+              })
+
+            }
+          } // end of responseObject
+        )
+      });
+    } catch (e) {
+      console.log(e);
+      res.json({status: false, body: {error: "message"}}) // all fails
+    }
+
+  });
+  ///////////////////
+
+  router.route('/test')
+  .get(function(req, res) {
+    try {
+      models.sequelize.sync(/*{force:true}*/).then(() => {
+        db.getRoomItems(1)
+        .then(aa => res.json({ status: true, body: aa }))
+      });
+    } catch (e) {
+      console.log(e);
+      res.json({ status: false, body: {error: "message"} });
+    }
+
+  });
 
 }
