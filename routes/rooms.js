1fb862bb0a64d57a9e69f0a3606fbb880e0225f9
Greg <houle.greg@gmail.com>
2016-12-05 18:37:32 -0500

    rooms endpoint changes (unfinished)
    Signed-off-by: Greg <houle.greg@gmail.com>

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,9 +1,9 @@
 module.exports = function(app, router, db) {
 
-  // /rooms/:room_type/:room_id' room_id is optional (changes behaviour of HTTP verbs)
+    // /rooms/:room_type/:room_id' room_id is optional (changes behaviour of HTTP verbs)
 
-  router.route('/rooms')
-    .get(function(req, res) {
+    router.route('/rooms')
+        .get(function(req, res) {
 
             var filters = req.query;
             if (filters.date_start || filters.date_end) {
@@ -21,18 +21,18 @@ module.exports = function(app, router, db) {
                     status: true,
                     body: rooms
                 }));
-    })
-    .post(function(req, res) {
-      //add item to DB with req.params.room_type and req.params.room_id
-      // check the body for all the parameter. Needs to be sanitized.
-      // check for Resources in the room.
-      db.addResource(req.body.room)
-        .then(resp => res.json({status: true, body: "Room created"}))
-    });
+        })
+        .post(function(req, res) {
+            //add item to DB with req.params.room_type and req.params.room_id
+            // check the body for all the parameter. Needs to be sanitized.
+            // check for Resources in the room.
+            db.addResource(req.body.room)
+                .then(resp => res.json({ status: true, body: "Room created" }))
+        });
 
-  router.route('/rooms/:room_type')
-    .get(function(req, res) {
-      //return room with req.params.room_type
+    router.route('/rooms/:room_type')
+        .get(function(req, res) {
+            //return room with req.params.room_type
 
             var filters = req.query;
             if (filters.date_start || filters.date_end) {
@@ -49,12 +49,12 @@ module.exports = function(app, router, db) {
                     status: true,
                     body: aa
                 }))
-    });
+        });
 
     router.route('/rooms/items/:room_id') // REFACTOR: we want consistency, :room_id should come after items
-      .get(function(req, res) {
-        //return room with req.params.room_type and req.params.room_id
-        db.getRoomItems(req.params.room_id)
+        .get(function(req, res) {
+            //return room with req.params.room_type and req.params.room_id
+            db.getRoomItems(req.params.room_id)
                 .then(items => res.json({
                     status: true,
                     body: items
@@ -64,9 +64,9 @@ module.exports = function(app, router, db) {
 
 
     router.route('/rooms/:room_type/:room_id')
-    .get(function(req, res) {
-      //return room with req.params.room_type and req.params.room_id
-      // REFACTOR: technically an ID should be unique, the room type should not be needed to retrieve by id
+        .get(function(req, res) {
+            //return room with req.params.room_type and req.params.room_id
+            // REFACTOR: technically an ID should be unique, the room type should not be needed to retrieve by id
 
             var filters = req.query;
             if (filters.date_start || filters.date_end) {
@@ -76,80 +76,81 @@ module.exports = function(app, router, db) {
                 filters.date_end = {
                     $lt: filters.date_end
                 };
-      }
+            }
             db.getRoomById( /* filters, */ req.params.room_id)
                 .then(room => res.json({
                     status: true,
                     body: room
                 }));
         })
-    .post(function(req,res){
-      db.updateResource(req.params.room_id,req.body.room).then(result => {
-      if(result){
+        .post(function(req, res) {
+            db.updateResource(req.params.room_id, req.body.room).then(result => {
+                if (result) {
 
-        res.json({ status: true, body: "Successfully modified Room." });
-      }
-      else{
-        res.json({ status: false, body: "There was an error in modifiying Room." });
-      }})
-    })
-    .delete(function(req, res) {
-      //Delete item from DB with req.params.room_type and req.params.room_id
+                    res.json({ status: true, body: "Successfully modified Room." });
+                } else {
+                    res.json({ status: false, body: "There was an error in modifiying Room." });
+                }
+            })
+        })
+        .delete(function(req, res) {
+            //Delete item from DB with req.params.room_type and req.params.room_id
             db.removeRoom(req.params.room_id)
                 .then(resp => res.json({
                     status: true,
                     body: resp
                 }))
-    });
+        });
 
 
-  router.route('/rooms/:room_id/:resource_type/:resource_id')
-    .post(function(req, res) {
-      // post only used to modify resources in a room, not to create new resources
-      // we don't need this path, the POST body will contain resource mods
-      // the path: '/rooms/items/:resource_id'
-      db.addRoomItem(req.params.room_id, req.params.resource_id)
+    router.route('/rooms/:room_id/:resource_type/:resource_id')
+        .post(function(req, res) {
+            // post only used to modify resources in a room, not to create new resources
+            // we don't need this path, the POST body will contain resource mods
+            // the path: '/rooms/items/:resource_id'
+            db.addRoomItem(req.params.room_id, req.params.resource_id)
                 .then(resp => res.json({
                     status: true,
                     body: resp
                 }))
-    })
-    .delete(function(req, res) {
-      // delete an item from a room, does not delete the item.
-      // path '/rooms/items/:room_id' and resource id in POST body. (something like that)
-      db.removeRoomItem(req.params.resource_id, req.params.room_id)
+        })
+        .delete(function(req, res) {
+            // delete an item from a room, does not delete the item.
+            // path '/rooms/items/:room_id' and resource id in POST body. (something like that)
+            db.removeRoomItem(req.params.resource_id, req.params.room_id)
                 .then(resp => res.json({
                     status: true,
                     body: resp
                 }))
-    });
+        });
 
 
     router.route('/rooms/reserve')
         .post(function(req, res) {
+            result = { "equipments": []}
 
             db.addReservation(req.body)
-                .then(rooms => res.json({
-                    status: true,
-                    body: rooms
-                }));
-
-            if (req.body.equipments) {
-                for (var resource in req.body.equipments) {
-                    var resourceBody = {
-                        "resource_id": resource.resource_id,
-                        "date_start": req.body.date_start,
-                        "date_end": req.body.date_end,
-                        "room_id": req.body.room_id,
-                        "user_id": req.body.user_id
+                .then(rooms => result["reserve_id"] = rooms.reserveId).then(() => {
+                    var equipments = []
+                    if (req.body.equipments) {
+                        for (var resource in req.body.equipments) {
+                            var resourceBody = {
+                                "resourceId": resource.resource_id
+                            }
+                            equipments.push(db.addResourceReservation(resourceBody, result.reserve_id));
+                        }
                     }
-                    db.addResourceReservation(resourceBody, null) // not sure about second param
-                        .then(resp => res.json({
-                            status: true,
-                            body: resp
-                        }));
-                }
-            }
+                    return Promise.all(equipments); //crash if one fails
+                }).then(  equipments => {
+                    for(var equip in equipments) {
+                        if (!equip.status) {
+                            //TODO
+                        }
+                    }
+                    res.json({
+                        status: true,
+                        body: result})
+                });
 
         });
 
