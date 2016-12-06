045a1470b68cd81c787bad04b27590b9ed7fa1eb
James St-Pierre <James@Jamess-iMac.local>
2016-12-05 22:23:30 -0500

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	.gitignore

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(bodyParser.json());
 // Allow origins
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "http://localhost:8080/");
-    res.header('Access-Control-Allow-Origin', "http://localhost:8081/");
+    res.header('Access-Control-Allow-Origin', "http://localhost:8081");
+    res.header('Access-Control-Allow-Origin', "http://162.253.53.97/");
     res.header('Access-Control-Allow-Credentials', "true");
     res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
