ee8f917792ee9f64bab4397a6aa148d1c882579b
Greg <houle.greg@gmail.com>
2016-12-06 02:58:32 -0500

    Fixed reservations (GET all GET unique)
    Signed-off-by: Greg <houle.greg@gmail.com>

--- routes/reservations.js
+++ routes/reservations.js
@@ -2,7 +2,7 @@ module.exports = function(app, router, db) {
 
     router.route('/reservations')
         .get(function(req, res) {
-            //return all users
+            /*
             var filters = req.query;
             if (filters.date_start || filters.date_end) {
                 filters.date_start = {
@@ -12,71 +12,51 @@ module.exports = function(app, router, db) {
                     $lt: filters.date_end
                 };
             }
+            */
 
-            try {
-                db.getReservations( /*filters*/ )
-                    .then(reservations => {
-
-                        if (reservations == null) {
-                            res.json({
-                                status: true,
-                                body: {
-                                    error: "No reservations found"
-                                }
-                            })
+            db.getReservations( /*filters*/ )
+                .then(reservations => {
+                    if (reservations.status) {
+                        if (reservations.body) {
+                            result = { reservations: [] }
+                            for (r of reservations.body) {
+                                r.date_start = Date.parse(r.start_time);
+                                r.date_end = Date.parse(r.end_time);
+                                delete r.start_time;
+                                delete r.end_time;
+                                result.reservations.push(r);
+                            }
+                            res.json({ status: true, body: result });
                         } else {
-                            res.json({
-                                status: true,
-                                body: reservations
-                            })
+                            res.json({ status: true, body: { reservations: [], message: "No reservations found" } });
                         }
-
-                    })
-            } catch (e) {
-                res.json({
-                    status: false,
-                    body: {
-                        error: "No reservations found"
+                    } else {
+                        res.json({ status: false, body: { error: "Retrieving reservations failed." } });
                     }
                 })
-            }
-
-
         })
 
     router.route('/reservations/:reservation_id')
         .get(function(req, res) {
-            //return all users
 
-            try {
-                db.getReservationById(req.params.reservation_id)
-                    .then(reservation => {
-
-                        if (reservation == null) {
-                            res.json({
-                                status: true,
-                                body: {
-                                    error: "No reservation found"
-                                }
-                            })
+            db.getReservationById(req.params.reservation_id)
+                .then(reservation => {
+                    if (reservation.status) {
+                        if (reservation.body) {
+                            result = { reservation: {}}
+                            reservation.body.start_date = Date.parse(reservation.body.start_time);
+                            reservation.body.end_date = Date.parse(reservation.body.end_time);
+                            delete reservation.body.start_time;
+                            delete reservation.body.end_time;
+                            result.reservation = reservation.body;
+                            res.json({status: true, body: result});
                         } else {
-                            res.json({
-                                status: true,
-                                body: reservation
-                            })
+                            res.json({ status: true, body: { reservation: {}, message: "No reservation found." } });
                         }
-
-                    })
-            } catch (e) {
-                res.json({
-                    status: false,
-                    body: {
-                        error: "No reservation found"
+                    } else {
+                        res.json({ status: false, body: { error: "Failed to Retrieve specific reservation." } });
                     }
-                })
-            }
-
-
-        })
+                });
+        });
 
 }
